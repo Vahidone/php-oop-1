b5d5f6a261b95101ff25ab6e7cfc2c2a52cc60f8
Vahidone <v.ban.roma@gmail.com>
2023-11-06 17:54:41 +0100

    text: update list of movies

--- db.php
+++ db.php
@@ -19,36 +19,36 @@ array_push(
         ['commedia', 'animazione']
     ),
 
-    $la_dolce_vita = new Movie(
-        'La dolce vita',
-        'Un classico del cinema italiano del 1960, esplora la vita mondana e vuota  di un giornalista romano in mezzo alla celebrità, al lusso e alla disillusione.',
+    $Django_Unchained = new Movie(
+        'Django Unchained',
+        'Un avvincente western che segue la vendetta di uno schiavo liberato e di un cacciatore di taglie nel profondo sud degli Stati Uniti.',
         'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
-        ['Marcello Mastroianni', 'Anita Ekberg'],
-        ['satirico', 'dramma']
+        ['Jamie Foxx:', 'Christoph Waltz'],
+        ['western', 'dramma']
     ),
 
-    $la_dolce_vita = new Movie(
-        'La dolce vita',
-        'Un classico del cinema italiano del 1960, esplora la vita mondana e vuota  di un giornalista romano in mezzo alla celebrità, al lusso e alla disillusione.',
+    $Baarìa = new Movie(
+        'Baarìa',
+        'Un film drammatico che racconta la storia di una famiglia siciliana attraverso tre generazioni, mostrando gli eventi sociali e politici che plasmano la loro vita nel corso del XX secolo.',
         'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
-        ['Marcello Mastroianni', 'Anita Ekberg'],
-        ['satirico', 'dramma']
+        ['Francesco Scianna', 'Margareth Madè'],
+        ['storico', 'dramma']
     ),
 
-    $la_dolce_vita = new Movie(
-        'La dolce vita',
-        'Un classico del cinema italiano del 1960, esplora la vita mondana e vuota  di un giornalista romano in mezzo alla celebrità, al lusso e alla disillusione.',
+    $Papillon = new Movie(
+        'Papillon',
+        'Un coinvolgente film drammatico che narra la storia di un uomo ingiustamente condannato per omicidio e del suo tentativo di fuga da un penitenziario di massima sicurezza.',
         'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
-        ['Marcello Mastroianni', 'Anita Ekberg'],
-        ['satirico', 'dramma']
+        ['Steve McQueen', 'Dustin Hoffman'],
+        ['avventura', 'dramma']
     ),
 
-    $la_dolce_vita = new Movie(
-        'La dolce vita',
-        'Un classico del cinema italiano del 1960, esplora la vita mondana e vuota  di un giornalista romano in mezzo alla celebrità, al lusso e alla disillusione.',
+    $Pulp_Fiction = new Movie(
+        'Pulp Fiction',
+        'Un audace film noir che intreccia le storie di vari personaggi disfunzionali nel mondo del crimine, offrendo una narrazione non lineare e dialoghi memorabili.',
         'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
-        ['Marcello Mastroianni', 'Anita Ekberg'],
-        ['satirico', 'dramma']
+        ['John Travolta', 'Samuel L. Jackson'],
+        ['commedia', 'noir']
     )
 
     
