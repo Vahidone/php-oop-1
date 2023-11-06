faad7da0471ddd4cc964abf508fcf2eb7b9773ea
Vahidone <v.ban.roma@gmail.com>
2023-11-06 18:07:17 +0100

    feat: change image of movies

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ array_push(
     $la_dolce_vita = new Movie(
         'La dolce vita',
         'Un classico del cinema italiano del 1960, esplora la vita mondana e vuota  di un giornalista romano in mezzo alla celebrità, al lusso e alla disillusione.',
-        'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
+        'https://pad.mymovies.it/filmclub/2002/08/069/locandina.jpg',
         ['Marcello Mastroianni', 'Anita Ekberg'],
         ['satirico', 'dramma']
     ),
@@ -14,7 +14,7 @@ array_push(
     $Ratatouille = new Movie(
         'Ratatouille',
         'Un affascinante film di animazione che narra la storia di un topo appassionato di cucina che diventa uno chef di talento in un rinomato ristorante parigino.',
-        'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
+        'https://m.media-amazon.com/images/I/712nXuCRhaL._AC_UF1000,1000_QL80_.jpg',
         ['Patton Oswalt', 'Lou Romano'],
         ['commedia', 'animazione']
     ),
@@ -22,7 +22,7 @@ array_push(
     $Django_Unchained = new Movie(
         'Django Unchained',
         'Un avvincente western che segue la vendetta di uno schiavo liberato e di un cacciatore di taglie nel profondo sud degli Stati Uniti.',
-        'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
+        'https://m.media-amazon.com/images/I/61tTz-WhbdL._AC_UF1000,1000_QL80_.jpg',
         ['Jamie Foxx:', 'Christoph Waltz'],
         ['western', 'dramma']
     ),
@@ -30,7 +30,7 @@ array_push(
     $Baarìa = new Movie(
         'Baarìa',
         'Un film drammatico che racconta la storia di una famiglia siciliana attraverso tre generazioni, mostrando gli eventi sociali e politici che plasmano la loro vita nel corso del XX secolo.',
-        'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
+        'https://www.etriggerz.com/cdn/shop/products/AB47980_grande.jpeg?v=1521760931',
         ['Francesco Scianna', 'Margareth Madè'],
         ['storico', 'dramma']
     ),
@@ -38,7 +38,7 @@ array_push(
     $Papillon = new Movie(
         'Papillon',
         'Un coinvolgente film drammatico che narra la storia di un uomo ingiustamente condannato per omicidio e del suo tentativo di fuga da un penitenziario di massima sicurezza.',
-        'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
+        'https://c8.alamy.com/compit/p6rrmd/papillon-anno-1973-usa-direttore-franklin-j-schaffner-steve-mcqueen-dustin-hoffman-poster-fr-p6rrmd.jpg',
         ['Steve McQueen', 'Dustin Hoffman'],
         ['avventura', 'dramma']
     ),
@@ -46,7 +46,7 @@ array_push(
     $Pulp_Fiction = new Movie(
         'Pulp Fiction',
         'Un audace film noir che intreccia le storie di vari personaggi disfunzionali nel mondo del crimine, offrendo una narrazione non lineare e dialoghi memorabili.',
-        'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
+        'https://m.media-amazon.com/images/I/718LfFW+tIL._AC_UF1000,1000_QL80_.jpg',
         ['John Travolta', 'Samuel L. Jackson'],
         ['commedia', 'noir']
     )
