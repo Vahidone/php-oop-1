d74644933a46e950e7463d7a13ff4234aaf75860
Vahidone <v.ban.roma@gmail.com>
2023-11-06 15:46:01 +0100

    feat: push a film in $movies list array

--- Models/db.php
+++ Models/db.php
@@ -1,5 +1,16 @@
 <?php
-$movies = [];
+  $movies = [];
+
+  array_push(
+    $movies,
+    $matrix = new Movie(
+      'Matrix',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum est?',
+      'https://cdn.pixabay.com/photo/2016/01/22/08/20/film-1155439_1280.jpg',
+      ['claudio bisio', 'lino banfi'],
+      ['action', 'drama']
+    ),
+  );
 
 
 
