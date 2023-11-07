b40b63dc3ece10b9f7c5a189b17073cd81e2b362
Vahidone <v.ban.roma@gmail.com>
2023-11-07 15:57:19 +0100

    feat: update file db.php

--- db.php
+++ db.php
@@ -1,60 +1,26 @@
 <?php
-$movies = [];
+require_once 'Models/Movie.php';
+require_once 'Models/TvSerie.php';
 
-array_push(
-    $movies,
+$productions = [
     new Movie(
         'La dolce vita',
-        'Un classico del cinema italiano del 1960, esplora la vita mondana e vuota  di un giornalista romano in mezzo alla celebrità, al lusso e alla disillusione.',
+        'Un classico del cinema italiano del 1960...',
         'https://pad.mymovies.it/filmclub/2002/08/069/locandina.jpg',
         ['Marcello Mastroianni', 'Anita Ekberg'],
-        ['satirico', 'dramma']
+        ['satirico', 'dramma'],
+        1960,
+        174
     ),
-
-    new Movie(
-        'Ratatouille',
-        'Un affascinante film di animazione che narra la storia di un topo appassionato di cucina che diventa uno chef di talento in un rinomato ristorante parigino.',
-        'https://m.media-amazon.com/images/I/712nXuCRhaL._AC_UF1000,1000_QL80_.jpg',
-        ['Patton Oswalt', 'Lou Romano'],
-        ['commedia', 'animazione']
+    new TvSerie(
+        'Friends',
+        'Una serie televisiva comica che segue...',
+        'https://www.friends-tv.org/graphics/season3.jpg',
+        ['Jennifer Aniston', 'Courteney Cox'],
+        ['commedia', 'romantico'],
+        1994,
+        2004,
+        236,
+        10
     ),
-
-    new Movie(
-        'Django Unchained',
-        'Un avvincente western che segue la vendetta di uno schiavo liberato e di un cacciatore di taglie nel profondo sud degli Stati Uniti.',
-        'https://m.media-amazon.com/images/I/61tTz-WhbdL._AC_UF1000,1000_QL80_.jpg',
-        ['Jamie Foxx:', 'Christoph Waltz'],
-        ['western', 'dramma']
-    ),
-
-    new Movie(
-        'Baarìa',
-        'Un film drammatico che racconta la storia di una famiglia siciliana attraverso tre generazioni, mostrando gli eventi sociali e politici che plasmano la loro vita nel corso del XX secolo.',
-        'https://www.etriggerz.com/cdn/shop/products/AB47980_grande.jpeg?v=1521760931',
-        ['Francesco Scianna', 'Margareth Madè'],
-        ['storico', 'dramma']
-    ),
-
-    new Movie(
-        'Papillon',
-        'Un coinvolgente film drammatico che narra la storia di un uomo ingiustamente condannato per omicidio e del suo tentativo di fuga da un penitenziario di massima sicurezza.',
-        'https://c8.alamy.com/compit/p6rrmd/papillon-anno-1973-usa-direttore-franklin-j-schaffner-steve-mcqueen-dustin-hoffman-poster-fr-p6rrmd.jpg',
-        ['Steve McQueen', 'Dustin Hoffman'],
-        ['avventura', 'dramma']
-    ),
-
-    new Movie(
-        'Pulp Fiction',
-        'Un audace film noir che intreccia le storie di vari personaggi disfunzionali nel mondo del crimine, offrendo una narrazione non lineare e dialoghi memorabili.',
-        'https://m.media-amazon.com/images/I/718LfFW+tIL._AC_UF1000,1000_QL80_.jpg',
-        ['John Travolta', 'Samuel L. Jackson'],
-        ['commedia', 'noir']
-    )
-
-    
-
-    
-
-    
-
-);
+];
